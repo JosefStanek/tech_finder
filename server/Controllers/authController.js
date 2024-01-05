39d7d3a06b8f9b007267c5c2ff43cd9af992be3c
Josef Staněk <101967039+JosefStanek@users.noreply.github.com>
2024-01-05 15:41:10 +0100

    Update authController.js

--- server/Controllers/authController.js
+++ server/Controllers/authController.js
@@ -19,12 +19,8 @@ export const registerController = async (req, res, next) => {
     const token = createToken(user._id);
     return res
       .status(201)
-      .cookie("jwt", token, {
-        withCrdentials: true,
-        httpOnly: false,
-        maxAge: maxAge * 1000,
-      })
       .json({
+        token: token,
         user: user._id,
         created: true,
       });
